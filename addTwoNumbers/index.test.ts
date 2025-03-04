8f45caf9a0cb86912da5b77f49c0d2ccbc162ac1
Matt <mattfields+dev@pm.me>
2025-03-04 23:09:23 +1000

    handle BigInts

--- addTwoNumbers/index.test.ts
+++ addTwoNumbers/index.test.ts
@@ -1,4 +1,4 @@
-import { ListNode, addTwoNumbers, intToNode } from '.';
+import { ListNode, addTwoNumbers, arrToNode, intToNode } from '.';
 import { describe, expect, test } from 'bun:test';
 
 type TestCase = {
@@ -8,10 +8,27 @@ type TestCase = {
   comment: string;
 };
 
+const hugeNum1 = BigInt('1000000000000000000000000000001');
+const hugeNum2 = BigInt('1000000000000000000000000000001');
+
+const listNode0 = new ListNode(0); // 0
+const listNode1 = new ListNode(1); // 1
 const listNode123 = new ListNode(3, new ListNode(2, new ListNode(1))); // 123 as int
 const listNode456 = new ListNode(6, new ListNode(5, new ListNode(4))); // 456 as int
 const listNode789 = new ListNode(9, new ListNode(8, new ListNode(7))); // 789 as int
-const listNode0 = new ListNode(0); // 0
+
+// Some really huge integers
+// 1000000000000000000000000000001
+const listNodeReallyBig = arrToNode([
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 1,
+]);
+// 1000000000000000000000000000002
+const listNodeReallyBig2 = arrToNode([
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 2,
+]);
+
 const listNode1001 = new ListNode(
   1,
   new ListNode(0, new ListNode(0, new ListNode(1))) // 1001 as int
@@ -46,6 +63,13 @@ const testCases: TestCase[] = [
     ),
     comment: '789 + 1001 = 1790 -> L(0,9,7,1)',
   },
+  {
+    t1: listNodeReallyBig,
+    t2: listNode1,
+    expected: listNodeReallyBig2,
+    comment:
+      '1000000000000000000000000000001 + 1 = 1000000000000000000000000000002',
+  },
 ];
 
 describe('add two linked lists of integers', () => {
@@ -63,4 +87,17 @@ describe('number to ListNode', () => {
   test('456', () => expect(intToNode(456)).toMatchObject(listNode456));
   test('0', () => expect(intToNode(0)).toMatchObject(listNode0));
   test('1001', () => expect(intToNode(1001)).toMatchObject(listNode1001));
+  test('1001', () => expect(intToNode(1001)).toMatchObject(listNode1001));
+  test('1000000000000000000000000000001', () =>
+    expect(intToNode(hugeNum1)).toMatchObject(listNodeReallyBig));
+});
+
+describe('array to ListNode', () => {
+  test('[1,2,3]', () =>
+    expect(arrToNode([1, 2, 3])).toMatchObject(listNode123));
+  test('[4,5,6]', () =>
+    expect(arrToNode([4, 5, 6])).toMatchObject(listNode456));
+  test('[0]', () => expect(arrToNode([0])).toMatchObject(listNode0));
+  test('[1,0,0,1]', () =>
+    expect(arrToNode([1, 0, 0, 1])).toMatchObject(listNode1001));
 });
--- addTwoNumbers/index.ts
+++ addTwoNumbers/index.ts
@@ -12,38 +12,50 @@ export class ListNode {
   }
 }
 
-// Convert ListNode to integer
+// Convert ListNode to integer (BigInt)
 // Note order is reversed
 // eg:  ListNode[3,4,5] -> 543
-const nodeToInt = (head: ListNode | null): number => {
+const nodeToInt = (head: ListNode | null): bigint => {
   var strVal = '';
   var current = head;
   while (current) {
     strVal = current.val.toString() + strVal; // prepend the current node value to the string
     current = current.next;
   }
-  return Number(strVal);
+  return BigInt(strVal);
 };
 
-// Convert an integer number to a ListNode
-// eg: 123 -> ListNode[3,2,1]
-export const intToNode = (num: number): ListNode => {
+// Convert an integer array to a ListNode
+export const arrToNode = (arr: number[]): ListNode => {
   var node = new ListNode();
   var current: ListNode | null = node;
-  const numArr = num.toString().split(''); // split num to string array
   while (current) {
-    current.val = Number(numArr.pop()); // pop the last number into current node
-    current.next = numArr.length ? new ListNode() : null; // if we have more coming, add a new ListNode as next
+    current.val = Number(arr.pop()); // pop the last number into current node
+    current.next = arr.length ? new ListNode() : null; // if we have more coming, add a new ListNode as next
     current = current.next;
   }
   return node;
 };
 
-// Given two linked lists, add the first number of each and return as array.
+// Convert an integer number to a ListNode
+// eg: 123 -> ListNode[3,2,1]
+export const intToNode = (num: number | bigint): ListNode => {
+  const numArr = num
+    .toString()
+    .split('')
+    .map((x) => Number(x)); // split num to array
+  return arrToNode(numArr);
+};
+
+const sum = (n1: bigint, n2: bigint) => n1 + n2;
+
+// Add two numbers represented as reverse-order linked lists
 export function addTwoNumbers(
   l1: ListNode | null,
   l2: ListNode | null
 ): ListNode {
-  const sum = nodeToInt(l1) + nodeToInt(l2);
+  const s1 = nodeToInt(l1);
+  const s2 = nodeToInt(l2);
+  const sum = s1 + s2;
   return intToNode(sum);
 }
