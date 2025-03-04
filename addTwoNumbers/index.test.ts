7c90d6bd237a2ad2dabf2b64879e7964f63d619d
Matt <mattfields+dev@pm.me>
2025-03-04 20:10:07 +1000

    solution 1 working

--- addTwoNumbers/index.test.ts
+++ addTwoNumbers/index.test.ts
@@ -1,39 +1,66 @@
-import { ListNode, addTwoNumbers } from './index';
-import { expect, test } from 'bun:test';
+import { ListNode, addTwoNumbers, intToNode } from '.';
+import { describe, expect, test } from 'bun:test';
 
 type TestCase = {
   t1: ListNode | null;
   t2: ListNode | null;
-  expected: number;
+  expected: ListNode;
+  comment: string;
 };
+
+const listNode123 = new ListNode(3, new ListNode(2, new ListNode(1))); // 123 as int
+const listNode456 = new ListNode(6, new ListNode(5, new ListNode(4))); // 456 as int
+const listNode789 = new ListNode(9, new ListNode(8, new ListNode(7))); // 789 as int
+const listNode0 = new ListNode(0); // 0
+const listNode1001 = new ListNode(
+  1,
+  new ListNode(0, new ListNode(0, new ListNode(1))) // 1001 as int
+);
 
 const testCases: TestCase[] = [
   {
-    // Simple list
-    t1: new ListNode(12),
-    t2: new ListNode(3),
-    expected: 15,
+    // 123 + 456 = 579 -> L(9,7,5)
+    t1: listNode123,
+    t2: listNode456,
+    expected: new ListNode(9, new ListNode(7, new ListNode(5))),
+    comment: '123 + 456 = 579 -> L(9,7,5)',
   },
   {
-    // Simple list
-    t1: new ListNode(1),
-    t2: new ListNode(4),
-    expected: 5,
+    t1: listNode0,
+    t2: listNode0,
+    expected: listNode0,
+    comment: '0 + 0 = L(0)',
   },
-
   {
-    // Linked lists
-    t1: new ListNode(1, new ListNode(2, new ListNode(3))),
-    t2: new ListNode(10, new ListNode(10)),
-    expected: 26,
+    t1: listNode123,
+    t2: listNode0,
+    expected: listNode123,
+    comment: '123 + 0 = 123 -> L(3,2,1)',
+  },
+  {
+    t1: listNode789,
+    t2: listNode1001,
+    expected: new ListNode(
+      0,
+      new ListNode(9, new ListNode(7, new ListNode(1)))
+    ),
+    comment: '789 + 1001 = 1790 -> L(0,9,7,1)',
   },
 ];
 
-testCases.map((t, index) => {
-  test(`[${index + 1}/${
-    testCases.length
-  }] add two linked lists of integers`, () => {
-    const result = addTwoNumbers(t.t1, t.t2)?.val;
-    expect(result).toBe(t.expected);
+describe('add two linked lists of integers', () => {
+  testCases.map((t, index) => {
+    test(t.comment, () => {
+      console.log(`[${index + 1}/${testCases.length}] ${t.comment}`);
+      const result = addTwoNumbers(t.t1, t.t2);
+      expect(result).toMatchObject(t.expected);
+    });
   });
+});
+
+describe('number to ListNode', () => {
+  test('123', () => expect(intToNode(123)).toMatchObject(listNode123));
+  test('456', () => expect(intToNode(456)).toMatchObject(listNode456));
+  test('0', () => expect(intToNode(0)).toMatchObject(listNode0));
+  test('1001', () => expect(intToNode(1001)).toMatchObject(listNode1001));
 });
--- addTwoNumbers/index.ts
+++ addTwoNumbers/index.ts
@@ -12,17 +12,38 @@ export class ListNode {
   }
 }
 
-function sumNode(x: ListNode | null, acc: number = 0): number {
-  acc += x?.val || 0;
-  if (x?.next) {
-    return sumNode(x.next, acc);
+// Convert ListNode to integer
+// Note order is reversed
+// eg:  ListNode[3,4,5] -> 543
+const nodeToInt = (head: ListNode | null): number => {
+  var strVal = '';
+  var current = head;
+  while (current) {
+    strVal = current.val.toString() + strVal; // prepend the current node value to the string
+    current = current.next;
   }
-  return acc;
-}
+  return Number(strVal);
+};
 
+// Convert an integer number to a ListNode
+// eg: 123 -> ListNode[3,2,1]
+export const intToNode = (num: number): ListNode => {
+  var node = new ListNode();
+  var current: ListNode | null = node;
+  const numArr = num.toString().split(''); // split num to string array
+  while (current) {
+    current.val = Number(numArr.pop()); // pop the last number into current node
+    current.next = numArr.length ? new ListNode() : null; // if we have more coming, add a new ListNode as next
+    current = current.next;
+  }
+  return node;
+};
+
+// Given two linked lists, add the first number of each and return as array.
 export function addTwoNumbers(
   l1: ListNode | null,
   l2: ListNode | null
-): ListNode | null {
-  return new ListNode(sumNode(l1) + sumNode(l2));
+): ListNode {
+  const sum = nodeToInt(l1) + nodeToInt(l2);
+  return intToNode(sum);
 }
