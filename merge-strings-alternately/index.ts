30177cbf382e0b583991bdfe2ae25534fcbe2406
Matt <mattfields+dev@pm.me>
2025-03-05 15:16:05 +1000

    working solution

--- merge-strings-alternately/index.ts
+++ merge-strings-alternately/index.ts
@@ -1,1 +1,12 @@
-export function mergeAlternately(word1: string, word2: string): string {}
+export function mergeAlternately(word1: string, word2: string): string {
+  var merged = '';
+  let arr1 = word1.split('');
+  let arr2 = word2.split('');
+
+  while (arr1.length || arr2.length) {
+    let c1 = arr1.shift() || '';
+    let c2 = arr2.shift() || '';
+    merged = merged + c1 + c2;
+  }
+  return merged;
+}
