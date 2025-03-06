79595cdfeef0482ecf3830ed06a98b0c28d41c58
Matt <mattfields+dev@pm.me>
2025-03-06 17:46:32 +1000

    add stubs

---
+++ kids-greatest-number-candies/index.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect, test } from 'bun:test';
+import { kidsWithCandies } from '.';
+
+describe('check kids with highest number of candies', () => {
+  test('test always true', () => expect(true).toBeTrue());
+});
---
+++ kids-greatest-number-candies/index.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from 'bun:test';
+
+export function kidsWithCandies(
+  candies: number[],
+  extraCandies: number
+): boolean[] {
+  return [false];
+}
+
+describe('check kids with highest number of candies', () => {
+  test('test always true', () => expect(true).toBeTrue());
+});
