bb29ee5f187cb66faeda1d322444adda0f3f99a8
Matt <mattfields+dev@pm.me>
2025-03-08 15:11:41 +1000

    modify to use Map

--- two-sum/index.ts
+++ two-sum/index.ts
@@ -1,15 +1,13 @@
-type KeyMap = { [key: number]: number };
-
 export function twoSum(nums: number[], target: number): number[] {
-  const m: KeyMap = {};
+  const m = new Map<number, number>();
   for (let i = 0; i < nums.length; i++) {
-    let currentVal = nums[i];
-    let wantedVal = target - currentVal;
+    let val = nums[i];
+    let needed = target - val; // get index of matching set, if exists in the map
     // If the wanted value already in the map, return the index pair
-    if (wantedVal in m) {
-      return [m[wantedVal], i];
+    if (m.has(needed)) {
+      return [m.get(needed)!, i]; // ! means this will not be null; TS operator
     }
-    m[currentVal] = i; // Otherwise store index to map
+    m.set(val, i); // Otherwise store index to map
   }
   return [];
 }
