dd7e0fd9248d8d8449be76ecb8e56e4b49b3e6f5
Matt <mattfields+dev@pm.me>
2025-03-08 19:31:49 +1000

    math.min solution

--- min-stack/index.ts
+++ min-stack/index.ts
@@ -18,15 +18,7 @@ export class MinStack {
   }
 
   getMin(): number {
-    return this.stack.reduce((x, acc) => (x <= acc ? x : acc), this.stack[0]);
+    return Math.min(...this.stack);
   }
+  // return this.stack.reduce((x, acc) => (x <= acc ? x : acc), this.stack[0]);
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * var obj = new MinStack()
- * obj.push(val)
- * obj.pop()
- * var param_3 = obj.top()
- * var param_4 = obj.getMin()
- */
