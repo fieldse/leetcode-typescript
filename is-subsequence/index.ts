548fd89bf827eda9001f04ede052333253ce7c2d
Matt <mattfields+dev@pm.me>
2025-03-09 14:32:10 +1000

    working solution

--- is-subsequence/index.ts
+++ is-subsequence/index.ts
@@ -1,1 +1,11 @@
-export function isSubsequence(s: string, t: string): boolean {}
+export function isSubsequence(s: string, t: string): boolean {
+  const sChars = s.split('').reverse();
+  for (let i = 0; i < t.length; i++) {
+    let c1 = t[i]; // character from t
+    let c2 = sChars[sChars.length - 1]; // character from s
+    if (c1 === c2) {
+      sChars.pop();
+    }
+  }
+  return sChars.length === 0;
+}
