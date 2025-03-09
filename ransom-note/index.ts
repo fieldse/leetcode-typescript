fdaed4333701d011a57fb6b65103c43e5c4960e2
Matt <mattfields+dev@pm.me>
2025-03-09 16:25:33 +1000

    back to map solution

--- ransom-note/index.ts
+++ ransom-note/index.ts
@@ -1,17 +1,17 @@
 export function canConstruct(ransomNote: string, magazine: string): boolean {
-  // Create maps to get character counts of ransomNote and magazine
-  const m: Record<PropertyKey, number> = {};
+  // Use maps to get character count magazine
+  const m = new Map<string, number>();
   for (let char of magazine) {
-    m[char] = (m[char] || 0) + 1;
+    m.set(char, (m.get(char) || 0) + 1);
   }
-  // Iterate across note characters, removing one from magazine character count.
+  // Iterate across note characters, checking magazine character count,
   // If we get to negative, we've run out of letters.
   for (let char of ransomNote) {
-    let magCount = m[char] || 0;
+    let magCount = m.get(char) || 0;
     if (magCount === 0) {
       return false;
     }
-    m[char] = magCount - 1;
+    m.set(char, magCount - 1);
   }
   return true;
 }
