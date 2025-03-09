90f9bd67a2c2fba2b77f780803f1822404965cc9
Matt <mattfields+dev@pm.me>
2025-03-09 16:10:29 +1000

    object version

--- ransom-note/index.ts
+++ ransom-note/index.ts
@@ -1,28 +1,17 @@
-const mapChars = (word: string): Map<string, number> => {
-  const m = new Map<string, number>();
-  word.split('').map((char) => {
-    if (m.has(char)) {
-      let prev = m.get(char) as number;
-      m.set(char, prev + 1);
-    } else {
-      m.set(char, 1);
-    }
-  });
-  return m;
-};
-
 export function canConstruct(ransomNote: string, magazine: string): boolean {
   // Create maps to get character counts of ransomNote and magazine
-  const n = mapChars(ransomNote);
-  const m = mapChars(magazine);
-
-  // Iterate across note characters, ensuring we have at least its count in magazine characters
-  for (let char of n.keys()) {
-    let charCount = n.get(char)!;
-    let magCount = m.get(char);
-    if (!magCount || magCount < charCount) {
+  const m: Record<PropertyKey, number> = {};
+  for (let char of magazine) {
+    m[char] = (m[char] || 0) + 1;
+  }
+  // Iterate across note characters, removing one from magazine character count.
+  // If we get to negative, we've run out of letters.
+  for (let char of ransomNote) {
+    let magCount = m[char] || 0;
+    if (magCount === 0) {
       return false;
     }
+    m[char] = magCount - 1;
   }
   return true;
 }
