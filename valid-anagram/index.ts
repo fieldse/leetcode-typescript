6132f25f6ed1bad3cbdad0f1796767c4de1bb915
Matt <mattfields+dev@pm.me>
2025-03-09 18:54:02 +1000

    optimized solution

--- valid-anagram/index.ts
+++ valid-anagram/index.ts
@@ -1,3 +1,21 @@
-export function isAnagram(s: string, t: string): boolean {
+// Naive solution: very direct, but these sorts are time-costly
+export function isAnagram1(s: string, t: string): boolean {
   return s.split('').sort().toString() === t.split('').sort().toString();
+}
+
+// credit for this solution:
+// https://leetcode.com/problems/valid-anagram/solutions/6149327/typescript-solution-97
+export function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) {
+    return false;
+  }
+  let charCount = new Array(26).fill(0); // create an array for each letter of the alphabet
+  let a = 'a'.charCodeAt(0); // get charcode of 'a' as int
+  for (let i = 0; i < s.length; i++) {
+    let x = s.charCodeAt(i) - a;
+    let y = t.charCodeAt(i) - a;
+    charCount[x]++;
+    charCount[y]--;
+  }
+  return charCount.every((x) => x === 0);
 }
